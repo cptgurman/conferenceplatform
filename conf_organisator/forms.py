e0428d0ac7d06ff58fc8c0c4330c7c134d5511d8
kek <petuhov.dania1@yandex.ru>
2021-03-16 15:23:34 +0300

    недодизайн

--- conf_organisator/forms.py
+++ conf_organisator/forms.py
@@ -13,11 +13,17 @@ class ConferenceEditForm(ModelForm):
         model = Conference
         fields = "__all__"
         widgets = {
-        'conference_date_start': forms.SelectDateWidget,
-        'conference_date_end':forms.SelectDateWidget,
-        'conference_date_reg_start': forms.SelectDateWidget,
-        'conference_date_reg_end': forms.SelectDateWidget,
-        'org_id': forms.HiddenInput,
+            'conference_org_id': forms.HiddenInput(),
+            'conference_name': forms.TextInput(attrs={'class': 'konf'}),
+            'conference_full_name': forms.TextInput(attrs={'class': 'konf'}),
+            'conference_discription': forms.Textarea(attrs={'class': 'konf'}),
+            'conference_date_start': forms.SelectDateWidget(attrs={'class': 'data'}),
+            'conference_date_end':forms.SelectDateWidget(attrs={'class': 'data'}),
+            'conference_date_reg_start': forms.SelectDateWidget(attrs={'class': 'data'}),
+            'conference_date_reg_end': forms.SelectDateWidget(attrs={'class': 'data'}),
+            'conference_format_id': forms.Select(attrs={'class': 'konf'}),
+            'conference_faculty_id': forms.Select(attrs={'class': 'konf'}),
+            'conference_building_id': forms.Select(attrs={'class': 'konf'}),
         }
 
 
