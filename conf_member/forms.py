412c4088ce8c4d6449d2a230202d629c706e53bf
german_job <german1199@inbox.ru>
2021-04-21 11:32:08 +0300

    исправлены мелкие косяки

--- conf_member/forms.py
+++ conf_member/forms.py
@@ -1,8 +1,9 @@
 from django.forms import ModelForm, widgets
 from django.forms.fields import DateField
-from core.models import MemberInfo, Conference, MemberApplication
+from core.models import MemberInfo, Conference, MemberApplication, ConferenceSections
 from django import forms
 from django.forms.models import inlineformset_factory, modelform_factory, modelformset_factory
+import datetime
 
 
 class lkUser(ModelForm):
@@ -14,7 +15,7 @@ class lkUser(ModelForm):
             'memberinfo_surname': forms.TextInput(attrs={'class': 'member'}),
             'memberinfo_name': forms.TextInput(attrs={'class': 'member'}),
             'memberinfo_otchestvo': forms.TextInput(attrs={'class': 'member'}),
-            'memberinfo_dateofbirth': forms.SelectDateWidget(attrs={'class': 'data'}),
+            'memberinfo_dateofbirth': forms.SelectDateWidget(years=range(datetime.datetime.today().year-10,datetime.datetime.today().year-80,-1),attrs={'class': 'data'}),
             'memberinfo_telephone': forms.TextInput(attrs={'class': 'member'}),
             'memberinfo_job_telephone': forms.TextInput(attrs={'class': 'member'}),
             'memberinfo_dolshnost': forms.TextInput(attrs={'class': 'member'}),
@@ -49,6 +50,11 @@ class MemberCreateApplication(ModelForm):
             'hotel_required': forms.CheckboxInput(attrs={'class': 'application'}),
             'invitation_required': forms.CheckboxInput(attrs={'class': 'application'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        conference = Conference.objects.get(id=self.initial['conference_id']) 
+        self.fields['member_section'].queryset = ConferenceSections.objects.filter(conference_sections_conference_id=conference)
 
 
 class MemberUpdateApplication(ModelForm):
