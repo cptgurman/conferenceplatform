77c4a98ec8ec2e7aa94be0ade612fc90d3bacaf5
Davlure <petuhov.dania1@yandex.ru>
2021-05-08 00:05:06 +0300

    фончик

--- conf_member/urls.py
+++ conf_member/urls.py
@@ -6,12 +6,15 @@ from conf_member import views
 
 urlpatterns = [
     path('member_lk/', lk.as_view(template_name='conf_member/lk.html'), name="lk"),
-    path('member_data_update/<int:pk>', login_required(views.MemberDataUpdate.as_view(template_name='conf_member/MemberInfoUpdate.html')), name="MemberDataUpdate"),
-    path('member_data_create/', login_required(views.MemberDataCreate.as_view(template_name='conf_member/MemberInfoCreate.html')), name="MemberDataCreate"),
-    path('member_application_add/<int:conf_id>',login_required(MemberCreateApplicationView.as_view()), name='application_add'),
-    path('member_application_update/<int:pk>',login_required(MemberUpdateApplicationView.as_view()), name='application_update'),
-    path('member_confs/', login_required(MemberConfsList.as_view()), name='member_confs'),
+    path('member_data_update/<int:pk>', login_required(views.MemberDataUpdate.as_view(
+        template_name='conf_member/MemberInfoUpdate.html')), name="MemberDataUpdate"),
+    path('member_data_create/', login_required(views.MemberDataCreate.as_view(
+        template_name='conf_member/MemberInfoCreate.html')), name="MemberDataCreate"),
+    path('member_application_add/<int:conf_id>',
+         login_required(MemberCreateApplicationView.as_view()), name='application_add'),
+    path('member_application_update/<int:pk>',
+         login_required(MemberUpdateApplicationView.as_view()), name='application_update'),
+    path('member_confs/', login_required(MemberConfsList.as_view()),
+         name='member_confs'),
     path('searh/', Searh.as_view(), name='searh'),
-
-   
 ]
