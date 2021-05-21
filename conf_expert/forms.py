ad9b9ebdd31077cf458e179ef87f5b08570fc42c
Davlure <petuhov.dania1@yandex.ru>
2021-05-21 22:12:50 +0300

    css

--- conf_expert/forms.py
+++ conf_expert/forms.py
@@ -3,24 +3,24 @@ from core.models import MemberApplication, ExpertArticle
 from django import forms
 
 
-
 class ExpertReview(ModelForm):
     class Meta:
         model = MemberApplication
         fields = ['app_status', 'expert_app_comment']
         widgets = {
-             
+
             'app_status': forms.Select(attrs={'class': 'Review'}),
             'expert_app_comment': forms.Textarea(attrs={'class': 'Review'}),
 
         }
 
+
 class ExpertArticleUploadForm(ModelForm):
     class Meta:
-        model=ExpertArticle
-        fields= '__all__'
-        widgets= {
-            'expert':forms.HiddenInput(),
-            'article':forms.FileInput(attrs={'class': 'ExpertArticle'}),
-            'article_keywords':forms.Textarea(attrs={'class': 'ExpertArticle'}),
+        model = ExpertArticle
+        fields = '__all__'
+        widgets = {
+            'expert': forms.HiddenInput(),
+            'article': forms.FileInput(attrs={'class': 'article_file'}),
+            'article_keywords': forms.Textarea(attrs={'class': 'article_area', "placeholder": "Введите ключевые слова статьи"}),
         }
