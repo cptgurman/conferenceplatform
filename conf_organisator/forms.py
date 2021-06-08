6cfac7c4d23cee5c36037ff7ff6e9223f8aff2a5
Davlure <petuhov.dania1@yandex.ru>
2021-06-08 13:30:03 +0300

    полный фронт

--- conf_organisator/forms.py
+++ conf_organisator/forms.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm, widgets, modelformset_factory, formset_factory, inlineformset_factory
-from django.contrib.auth.forms import  UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.forms.fields import DateField
 from core.models import Conference, ConferenceSections
 from django import forms
-
 
 
 class ConferenceEditForm(ModelForm):
@@ -13,22 +12,21 @@ class ConferenceEditForm(ModelForm):
         fields = "__all__"
         widgets = {
             'conference_org_id': forms.HiddenInput(),
-            'conference_name': forms.TextInput(attrs={'class': 'konf'}),
-            'conference_full_name': forms.TextInput(attrs={'class': 'konf'}),
-            'conference_discription': forms.Textarea(attrs={'class': 'konf'}),
-            'conference_date_start': forms.SelectDateWidget(attrs={'class': 'data'}),
-            'conference_date_end':forms.SelectDateWidget(attrs={'class': 'data'}),
-            'conference_date_reg_start': forms.SelectDateWidget(attrs={'class': 'data'}),
-            'conference_date_reg_end': forms.SelectDateWidget(attrs={'class': 'data'}),
+            'conference_name': forms.TextInput(attrs={'class': 'konf', "placeholder": "Краткое название"}),
+            'conference_full_name': forms.TextInput(attrs={'class': 'konf', "placeholder": "Полное название"}),
+            'conference_discription': forms.Textarea(attrs={'class': 'konf', "placeholder": "Описание"}),
+            'conference_date_start': forms.SelectDateWidget(attrs={'class': 'conference_date'}),
+            'conference_date_end': forms.SelectDateWidget(attrs={'class': 'conference_date'}),
+            'conference_date_reg_start': forms.SelectDateWidget(attrs={'class': 'conference_date'}),
+            'conference_date_reg_end': forms.SelectDateWidget(attrs={'class': 'conference_date'}),
             'conference_format_id': forms.Select(attrs={'class': 'konf'}),
             'conference_faculty_id': forms.Select(attrs={'class': 'konf'}),
             'conference_building_id': forms.Select(attrs={'class': 'konf'}),
+            'conference_keywords': forms.Textarea(attrs={'class': 'konf', "placeholder": "Ключевые слова конференции"}),
         }
 
 
-
-ConferenceFormset = inlineformset_factory(Conference, ConferenceSections, fields = "__all__", widgets = {
-            'conference_sections_conference_id': forms.HiddenInput(),
-            'conference_sections_name': forms.TextInput(attrs={'class': 'konf'}),
-        }, extra=1)
-
+ConferenceFormset = inlineformset_factory(Conference, ConferenceSections, fields="__all__", widgets={
+    'conference_sections_conference_id': forms.HiddenInput(),
+    'conference_sections_name': forms.TextInput(attrs={'class': 'konf'}),
+}, extra=1)
